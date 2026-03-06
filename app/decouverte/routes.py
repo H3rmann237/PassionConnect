bccd5b0b4cfd4af10982db2043f1204fa3dae9b2
Hermann <samh3rmann@gmail.com>
2026-03-06 22:09:49 +0100

    page decouverte avec filtre

--- app/decouverte/routes.py
+++ app/decouverte/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, request
 from database import get_db
 
 decouverte = Blueprint('decouverte',__name__)
@@ -8,13 +8,30 @@ def page_decouverte():
     db = get_db()
     cursor = db.cursor()
 
+    toutes_passions = cursor.execute(
+        'SELECT * FROM passion'
+    ).fetchall()
+
     poste = cursor.execute('''
-        SELECT user.id, user.username, user.bio, GROUP_CONCAT(passion.icone || ' ' || passion.nom) as passions
+        SELECT user.id, user.username, user.bio, 
+            GROUP_CONCAT(passion.icone || ' ' || passion.nom) as passions
         FROM user
-        JOIN user_passion ON user.id = user_passion.user_id
-        JOIN passion ON passion.id = user_passion.passion_id
+        LEFT JOIN user_passion ON user.id = user_passion.user_id
+        LEFT JOIN passion ON passion.id = user_passion.passion_id
         GROUP BY user.id
     ''').fetchall()
-    db.close()
 
-    return render_template('decouverte.html', poste=poste)
+    passion_choisie = request.args.get('passion')
+
+    if passion_choisie:
+        poste = cursor.execute('''
+            SELECT user.id, user.username, user.bio, 
+                GROUP_CONCAT(passion.icone || ' ' || passion.nom) as passions
+            FROM user
+            LEFT JOIN user_passion ON user.id = user_passion.user_id
+            LEFT JOIN passion ON passion.id = user_passion.passion_id
+            WHERE passion.nom = ?
+            GROUP BY user.id
+        ''', (passion_choisie,)).fetchall()
+    db.close()
+    return render_template('decouverte.html', poste=poste, toutes_passions=toutes_passions)
