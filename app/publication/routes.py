e270ac8d09380efe744a50128ba224e7c357b4aa
Hermann <samh3rmann@gmail.com>
2026-03-12 20:09:54 +0100

    Ajout du systeme d'upload et affichage des images

--- app/publication/routes.py
+++ app/publication/routes.py
@@ -2,6 +2,9 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from database import get_db
 
+from werkzeug.utils import secure_filename
+import os
+
 publication = Blueprint('publication',__name__)
 
 @publication.route('/publication')
@@ -11,7 +14,7 @@ def mes_publications():
     cursor = db.cursor()
 
     publications = cursor.execute('''
-        SELECT publication.contenu, publication.created_at, user.username, user.id as user_id
+        SELECT publication.contenu,publication.image, publication.created_at, user.username, user.id as user_id
         FROM publication
         JOIN user ON publication.user_id = user.id
         ORDER BY publication.created_at DESC
@@ -25,14 +28,18 @@ def mes_publications():
 def new_publication():
     if request.method == 'POST':    
         contenu = request.form['contenu']
-        image = request.form['image']
+        image = request.files['image']
+        nom_fichier = None
+        if image.filename != '':
+            nom_fichier = secure_filename(image.filename)
+            image.save(os.path.join('app/static/uploads', nom_fichier))
 
         db = get_db()
         cursor = db.cursor()
         cursor.execute('''
             INSERT INTO publication (contenu, image, user_id)
             VALUES (?, ?, ?)
-        ''', (contenu,image, current_user.id))
+        ''', (contenu,nom_fichier, current_user.id))
         db.commit()
         db.close()
 
