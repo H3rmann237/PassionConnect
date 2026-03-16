8d304f3159e1851ed244a1d6e7238ab22aa4c3ef
Hermann <samh3rmann@gmail.com>
2026-03-16 22:40:12 +0100

    modification mineurs

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'bof'
+    SECRET_KEY = 'p6uDovY1YG3zcamU4mRD'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///passionconnect.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
